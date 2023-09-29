d0729bc53f3ad5ddd7c1fe3a78a3659d5754745e
tmegha1405 <117816288+tmegha1405@users.noreply.github.com>
2023-09-29 14:42:31 +0530

    Add files via upload

---
+++ boolean.py
@@ -0,0 +1,11 @@
+print("Demo of basic types:Boolean")
+t=True
+f=False
+print("t=",t)
+print("f=",f)
+print("Data types of variable t:",type(t))
+print("Data types of variable f:",type(f))
+print("Logical AND operations:",t and f)
+print("Logical OR operations:",t or f)
+print("Logical NOT operations:",not t)
+print("Logical XOR operations:",t!=f)
